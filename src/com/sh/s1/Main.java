0feade0e8673b558ac7dd813d8f75e671cf316c1
SeungHa <36650383+NohSeungHa@users.noreply.github.com>
2018-02-20 10:23:43 +0900

    Test1
    테스트

--- src/com/sh/s1/Main.java
+++ src/com/sh/s1/Main.java
@@ -3,6 +3,7 @@ package com.sh.s1;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("프로그램 스타트");
 		SchoolController scl = new SchoolController();
 		scl.start();
 
